5ab4f0921780c53b8627a3a3ca8c03e2a3fae567
Brian Rizzi <brian.rizzi@iisviolamarchesini.edu.it>
2023-10-04 19:32:11 +0200

    Carico prima versione esercizio conversioni

---
+++ Conversioni/Conversioni/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversioni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* FIRME METODI
+             * static bool[] ConvertDpToBin(int[] dp) METODO 1
+             * static int ConvertDpToIntero(int[] dp) METODO 2
+             * static int ConvertBinToIntero(bool[] bn)  METODO 3
+             * static int ConvertBinToDec(bool[] bn) METODO 4 
+             */
+            int[] dp = { 192, 168, 10, 1 };
+
+            bool[] b = ConverDpToBin(dp);
+
+            for (int i = 0; i < b.Length; i++)
+            {
+                Console.Write(Convert.ToInt32(b[i]));
+            }
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+
+        static bool[] ConverDpToBin(int[] dp)
+        {
+            bool[] b = new bool[32];
+            int n, j;
+
+            for (int i = 0; i < dp.Length; i++)
+            {
+                n = dp[i];
+                j = 7;
+                do
+                {
+                    if (n % 2 == 1)
+                    {
+                        b[j + i * 8] = true;
+                    }
+
+                    n = n / 2;
+                    j--;
+
+                } while (n > 0);
+            }
+
+            return b;
+        }
+    }
+}
